f16f83aeb2ce9474f50e1cb3160e895c725e150c
Paula Andrea Arroyave <paulaarroyavea@gmail.com>
2025-09-21 20:17:31 -0500

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import AuthLayout from "./layouts/auth-layout";
+import LoginPage from "./pages/login-page";
+import SignUpPage from "./pages/sign-up-page";
+import AppLayout from "./layouts/app-layout";
+import NotesPage from "./pages/notes-page";
 
 function App() {
-
   return (
-    <>
-    Hello tailwind
-    </>
-  )
+    <div className="flex items-center overflow-auto min-h-screen ">
+      <Routes>
+        <Route index element={<p>Home</p>} />
+        {/* <Route path="about" element={<About />} /> */}
+
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="sign-up" element={<SignUpPage />} />
+        </Route>
+
+        <Route element={<AppLayout />}>
+          <Route path="dashboard" element={<div>dashboard works</div>} />
+          <Route path="notes" element={<NotesPage />} />
+          <Route path="upload" element={<div>upload works</div>} />
+          <Route path="quizzes" element={<div>Quizzes works</div>} />
+          <Route path="profile" element={<div>Profile works</div>} />
+        </Route>
+      </Routes>
+      {/* <Route index element={<div>a</div>} /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
